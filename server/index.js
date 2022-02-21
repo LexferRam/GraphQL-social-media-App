e0e9cc6d07c06de15d99161d854f703687fc960a
Lexfer <lexferramirez@gmail.com>
2022-02-20 21:32:25 -0400

    register and login users

--- server/index.js
+++ server/index.js
@@ -1,40 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server')
 const mongoose = require('mongoose')
 
-const Post = require('./models/Post')
+const typeDefs = require('./graphql/typesDefs')
+const resolvers = require('./graphql/resolvers')
 const {MONGODB} = require('./config.js')
-
-const typeDefs = gql`
-    type Post{
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`;
-
-const resolvers = {
-    Query:{
-        async getPosts() {
-            try{
-                const posts = await Post.find();
-                return posts
-            }catch(err){
-                throw new Error(err)
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
 mongoose.connect(MONGODB, {useNewUrlParser: true}).then(() => {
+
     console.log('MongoDB Connected')
-    return server.listen({port:5000}).then(({ url }) => {
-        console.log(`🚀  Server ready at ${url}`);
-      });
+
+    return server
+            .listen({port:5000})
+            .then(({ url }) => {
+                console.log(`🚀  Server ready at ${url}`);
+            });
 });
 
