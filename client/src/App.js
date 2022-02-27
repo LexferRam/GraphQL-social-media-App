df8d103a8c5c524d84262c41d75443eeb9e0acdb
Lexfer <lexferramirez@gmail.com>
2022-02-27 13:22:16 -0400

    [ADD] App routes and pages home, login, register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+// import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-  hi world
-    </div>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import ApolloProv from './ApolloProvider';
 
 ReactDOM.render(<ApolloProv />,document.getElementById('root'));
